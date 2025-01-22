bfd5082b2809152cbada9a39042bf4c4ac738521
Diego-Trapote <trapote048@gmail.com>
2025-01-22 17:49:31 -0300

    Cree una lista de amigos y una funcion para agregar a los amigos a la lista

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaAmigos = [];
+
+function agregarAmigo() {
+    let amigoNuevo = document.getElementById('amigo').value;
+    listaAmigos.push(amigoNuevo);
+    console.log(amigoNuevo);
+    console.log(listaAmigos);
+    limpiarCaja();
+}
+
+function limpiarCaja() {
+
+    document.getElementById('amigo').value = ' ';
+    
+}
